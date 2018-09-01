444fe3d755e604f69134d556832119f1d8d9a06d
Roland Weigelt <mail@roland-weigelt.de>
2018-09-01 23:57:08 +0200

    Added support for relative file paths for referencing resource dictionaries

--- ResMerger/ResourceMerger.cs
+++ ResMerger/ResourceMerger.cs
@@ -36,172 +36,231 @@ THE SOFTWARE.
 */
 namespace ResMerger
 {
-    /// <summary>
-    /// <para>
-    /// The class Data represents a container for Documents and their DependencyCount
-    /// </para>
-    /// </summary>
-    public class Data
-    {
-        public XDocument Document { get; set; }
-        public int DependencyCount { get; set; }
+	/// <summary>
+	/// <para>
+	/// The class Data represents a container for Documents and their DependencyCount
+	/// </para>
+	/// </summary>
+	public class Data
+	{
+		public XDocument Document { get; set; }
+		public int DependencyCount { get; set; }
 
-        public Data(XDocument document, int dependencyCount)
-        {
-            Document = document;
-            DependencyCount = dependencyCount;
-        }
-    }
+		public Data(XDocument document, int dependencyCount)
+		{
+			Document = document;
+			DependencyCount = dependencyCount;
+		}
+	}
 
-    /// <summary>
-    /// <para>
-    /// The class ResMerge copies all resource dictionary entries into one large file respecting the dependencies to increase performance.
-    /// </para>
-    /// </summary>
-    public static class ResourceMerger
-    {
-        public const string COUNT_EXCEPTION = "Param count must be >= 1 and < 4.";
-        public const int COUNT_EXCEPTION_CODE = 0;
+	/// <summary>
+	/// <para>
+	/// The class ResMerge copies all resource dictionary entries into one large file respecting the dependencies to increase performance.
+	/// </para>
+	/// </summary>
+	public static class ResourceMerger
+	{
+		public const string COUNT_EXCEPTION = "Param count must be >= 1 and < 4.";
+		public const int COUNT_EXCEPTION_CODE = 0;
 
-        public const string PROJECT_PATH_EXCEPTION = "Project path does not exist.";
-        public const int PROJECT_PATH_EXCEPTION_CODE = 1;
+		public const string PROJECT_PATH_EXCEPTION = "Project path does not exist.";
+		public const int PROJECT_PATH_EXCEPTION_CODE = 1;
 
-        public const string FILE_EXCEPTION = "File does not exist: ";
-        public const int FILE_EXCEPTION_CODE = 2;
+		public const string FILE_EXCEPTION = "File does not exist: ";
+		public const int FILE_EXCEPTION_CODE = 2;
 
-        public const string SOURCE_EXCEPTION = "Source file does not exist: ";
-        public const int SOURCE_EXCEPTION_CODE = 3;
+		public const string SOURCE_EXCEPTION = "Source file does not exist: ";
+		public const int SOURCE_EXCEPTION_CODE = 3;
 
-        public const string SOURCE_TYPE_EXCEPTION = "Wrong file type for source - must be .xaml";
-        public const int SOURCE_TYPE_EXCEPTION_CODE = 4;
+		public const string SOURCE_TYPE_EXCEPTION = "Wrong file type for source - must be .xaml";
+		public const int SOURCE_TYPE_EXCEPTION_CODE = 4;
 
-        public const string OUTPUT_TYPE_EXCEPTION = "Wrong file type for output - must be .xaml";
-        public const int OUTPUT_TYPE_EXCEPTION_CODE = 5;
+		public const string OUTPUT_TYPE_EXCEPTION = "Wrong file type for output - must be .xaml";
+		public const int OUTPUT_TYPE_EXCEPTION_CODE = 5;
 
-        private const string LOG_FILE_NAME = "ResMergerLog.txt";
+		private const string LOG_FILE_NAME = "ResMergerLog.txt";
 
-        /// <summary>
-        /// save all resources into one big resource dictionary respecting the dependencies to increase performance
-        /// </summary>
-        /// <param name="projectPath">project path (C:/..)</param>
-        /// <param name="relativeSourceFilePath">relative source file path (/LookAndFeel.xaml)</param>
-        /// <param name="relativeOutputFilePath">relative output file path (/xGeneric/Generic.xaml)</param>
-        /// <param name="projectName">project name</param>
-        /// <param name="resDictString">resource dictionary string (node name)</param>
-        public static void MergeResources(string projectPath, string projectName = null, string relativeSourceFilePath = "/LookAndFeel.xaml", string relativeOutputFilePath = "/FullLookAndFeel.xaml")
-        {
-            // if project path does not exist throw exception
-            if (!Directory.Exists(projectPath))
-                Helpers.ThrowException<Exception>(PROJECT_PATH_EXCEPTION);
+		/// <summary>
+		/// save all resources into one big resource dictionary respecting the dependencies to increase performance
+		/// </summary>
+		/// <param name="projectPath">project path (C:/..)</param>
+		/// <param name="relativeSourceFilePath">relative source file path (/LookAndFeel.xaml)</param>
+		/// <param name="relativeOutputFilePath">relative output file path (/xGeneric/Generic.xaml)</param>
+		/// <param name="projectName">project name</param>
+		/// <param name="resDictString">resource dictionary string (node name)</param>
+		public static void MergeResources(string projectPath, string projectName = null, string relativeSourceFilePath = "/LookAndFeel.xaml", string relativeOutputFilePath = "/FullLookAndFeel.xaml")
+		{
+			// if project path does not exist throw exception
+			if (!Directory.Exists(projectPath))
+				Helpers.ThrowException<Exception>(PROJECT_PATH_EXCEPTION);
 
-            // Get default values for optional parameters
-            projectName = string.IsNullOrEmpty(projectName) ? Path.GetFileName(Path.GetDirectoryName(projectPath)) : projectName;
+			// Get default values for optional parameters
+			projectName = string.IsNullOrEmpty(projectName) ? Path.GetFileName(Path.GetDirectoryName(projectPath)) : projectName;
 
-            // if relativeSourceFilePath is not of type .xaml throw exception
-            if (!relativeSourceFilePath.EndsWith(".xaml", StringComparison.InvariantCultureIgnoreCase))
-                Helpers.ThrowException<Exception>(SOURCE_TYPE_EXCEPTION);
+			// if relativeSourceFilePath is not of type .xaml throw exception
+			if (!relativeSourceFilePath.EndsWith(".xaml", StringComparison.InvariantCultureIgnoreCase))
+				Helpers.ThrowException<Exception>(SOURCE_TYPE_EXCEPTION);
 
-            // if relativeOutputFilePath is not of type .xaml throw exception
-            if (!relativeOutputFilePath.EndsWith(".xaml", StringComparison.InvariantCultureIgnoreCase))
-                Helpers.ThrowException<Exception>(OUTPUT_TYPE_EXCEPTION);
+			// if relativeOutputFilePath is not of type .xaml throw exception
+			if (!relativeOutputFilePath.EndsWith(".xaml", StringComparison.InvariantCultureIgnoreCase))
+				Helpers.ThrowException<Exception>(OUTPUT_TYPE_EXCEPTION);
 
-            // create sourceFilePath
-            var sourceFilePath = projectPath + relativeSourceFilePath;
+			// create sourceFilePath
+			var sourceFilePath = projectPath + relativeSourceFilePath;
 
-            // if source file does not exist throw exception
-            if (!File.Exists(sourceFilePath))
-                Helpers.ThrowException<Exception>(SOURCE_EXCEPTION + sourceFilePath);
+			// if source file does not exist throw exception
+			if (!File.Exists(sourceFilePath))
+				Helpers.ThrowException<Exception>(SOURCE_EXCEPTION + sourceFilePath);
 
-            // load source doc
-            var sourceDoc = XDocument.Load(sourceFilePath);
+			// load source doc
+			var sourceDoc = XDocument.Load(sourceFilePath);
 
-            // get default namespace for doc creation and filtering
-            var defaultNameSpace = sourceDoc.Root.GetDefaultNamespace();
+			// get default namespace for doc creation and filtering
+			var defaultNameSpace = sourceDoc.Root.GetDefaultNamespace();
 
-            // get res dict string
-            var resDictString = sourceDoc.Root.Name.LocalName;
+			// get res dict string
+			var resDictString = sourceDoc.Root.Name.LocalName;
 
-            // create output doc
-            var outputDoc = XDocument.Parse("<" + resDictString + " xmlns=\"" + defaultNameSpace + "\"/>");
+			// create output doc
+			var outputDoc = XDocument.Parse("<" + resDictString + " xmlns=\"" + defaultNameSpace + "\"/>");
 
-            // create documents
-            var documents = new Dictionary<string, Data>();
-          
-            // add elements
-            ResourceMerger.PrepareDocuments(ref documents, projectPath, projectName, relativeSourceFilePath);
-       
-            // add elements (ordered by dependency count)
-            foreach (var item in documents.OrderByDescending(item => item.Value.DependencyCount))
-            {
-                // add attributes
-                foreach (var attribute in item.Value.Document.Root.Attributes())
-                    outputDoc.Root.SetAttributeValue(attribute.Name, attribute.Value);
+			// create documents
+			var documents = new Dictionary<string, Data>();
 
-                // add elements
-                outputDoc.Root.Add(item.Value.Document.Root.Elements().Where(e => !e.Name.LocalName.StartsWith(resDictString)));
-            }
+			// add elements
+			ResourceMerger.PrepareDocuments(ref documents, projectPath, projectName, relativeSourceFilePath);
 
-            using (var ms = new MemoryStream())
-            {
-                outputDoc.Save(ms);
+			// add elements (ordered by dependency count)
+			foreach (var item in documents.OrderByDescending(item => item.Value.DependencyCount))
+			{
+				// add attributes
+				foreach (var attribute in item.Value.Document.Root.Attributes())
+					outputDoc.Root.SetAttributeValue(attribute.Name, attribute.Value);
 
-                if (OutputEqualsExistingFileContent(Path.Combine(projectPath, relativeOutputFilePath), ms.ToArray()))
-                    return;
-            }
+				// add elements
+				outputDoc.Root.Add(item.Value.Document.Root.Elements().Where(e => !e.Name.LocalName.StartsWith(resDictString)));
+			}
 
-            // save file
-            outputDoc.Save(projectPath + relativeOutputFilePath);
-        }
+			using (var ms = new MemoryStream())
+			{
+				outputDoc.Save(ms);
 
-        private static bool OutputEqualsExistingFileContent(string targetFileName, IEnumerable<byte> newFileContent)
-        {
-            var normalizedFileName = targetFileName.Replace("/", "\\");
+				if (OutputEqualsExistingFileContent(Path.Combine(projectPath, relativeOutputFilePath), ms.ToArray()))
+					return;
+			}
 
-            // don't check for equality if the FullLookAndFeel.xaml doesn't exist already
-            if (!File.Exists(normalizedFileName))
-                return false;
+			// save file
+			outputDoc.Save(projectPath + relativeOutputFilePath);
+		}
 
-            var existingFileContentBytes = File.ReadAllBytes(normalizedFileName);
-            return newFileContent.SequenceEqual(existingFileContentBytes); ;
-        }
+		private static bool OutputEqualsExistingFileContent(string targetFileName, IEnumerable<byte> newFileContent)
+		{
+			var normalizedFileName = targetFileName.Replace("/", "\\");
 
-        /// <summary>
-        /// Get a collection of resource dictionary source paths respecting the dependencies 
-        /// </summary>
-        /// <param name="documents">output document collection</param>
-        /// <param name="projectPath">project path</param>
-        /// <param name="projectName">project name</param>
-        /// <param name="relativeSourceFilePath">relative source file path</param>
-        /// <param name="resDictString">resource dictionary string (node name)</param>
-        /// <param name="firstTime">first time, is LookAndFeel?</param>
-        /// <param name="parentDependencyCount">dependency count</param>
-        private static void PrepareDocuments(ref Dictionary<string, Data> documents, string projectPath, string projectName, string relativeSourceFilePath, bool firstTime = true, int parentDependencyCount = 0)
-        {
-            // load current doc
-            var absoluteSourceFilePath = projectPath + relativeSourceFilePath;
+			// don't check for equality if the FullLookAndFeel.xaml doesn't exist already
+			if (!File.Exists(normalizedFileName))
+				return false;
 
-            // if file does not exist throw exception
-            if (!File.Exists(absoluteSourceFilePath))
-                Helpers.ThrowException<Exception>(FILE_EXCEPTION + absoluteSourceFilePath);
+			var existingFileContentBytes = File.ReadAllBytes(normalizedFileName);
+			return newFileContent.SequenceEqual(existingFileContentBytes); ;
+		}
 
-            // load the doc
-            var doc = XDocument.Load(absoluteSourceFilePath);
+		/// <summary>
+		/// Get a collection of resource dictionary source paths respecting the dependencies 
+		/// </summary>
+		/// <param name="documents">output document collection</param>
+		/// <param name="projectPath">project path</param>
+		/// <param name="projectName">project name</param>
+		/// <param name="relativeSourceFilePath">relative source file path</param>
+		/// <param name="resDictString">resource dictionary string (node name)</param>
+		/// <param name="firstTime">first time, is LookAndFeel?</param>
+		/// <param name="parentDependencyCount">dependency count</param>
+		private static void PrepareDocuments(ref Dictionary<string, Data> documents, string projectPath, string projectName, string relativeSourceFilePath, bool firstTime = true, int parentDependencyCount = 0)
+		{
+			// load current doc
+			var absoluteSourceFilePath = Path.GetFullPath(Path.Combine(projectPath, relativeSourceFilePath));
 
-            // get the corresponding res dict name
-            var resDictString = doc.Root.Name.LocalName;
+			// if file does not exist throw exception
+			if (!File.Exists(absoluteSourceFilePath))
+				Helpers.ThrowException<Exception>(FILE_EXCEPTION + absoluteSourceFilePath);
 
-            // get default namespace
-            var defaultNameSpace = doc.Root.GetDefaultNamespace();
+			// load the doc
+			var doc = XDocument.Load(absoluteSourceFilePath);
 
-            // if key already added increase dependency count else add item with dependency count set to 0
-            if (documents.ContainsKey(absoluteSourceFilePath))
-                documents[absoluteSourceFilePath].DependencyCount = Math.Max(documents[absoluteSourceFilePath].DependencyCount + 1, parentDependencyCount + 1);
-            else
-                documents.Add(absoluteSourceFilePath, new Data(doc, firstTime ? -1 : parentDependencyCount + 1));
+			// get the corresponding res dict name
+			var resDictString = doc.Root.Name.LocalName;
 
-            // call PrepareDocuments() for each merged dictionary
-            foreach (var dict in doc.Root.Descendants(defaultNameSpace + resDictString))
-                PrepareDocuments(ref documents, projectPath, projectName, dict.Attribute("Source").Value.Replace("/" + projectName + ";component/", string.Empty), false, documents[absoluteSourceFilePath].DependencyCount);
-        }
-    }
+			// get default namespace
+			var defaultNameSpace = doc.Root.GetDefaultNamespace();
+
+			// if key already added increase dependency count else add item with dependency count set to 0
+			if (documents.ContainsKey(absoluteSourceFilePath))
+				documents[absoluteSourceFilePath].DependencyCount = Math.Max(documents[absoluteSourceFilePath].DependencyCount + 1, parentDependencyCount + 1);
+			else
+				documents.Add(absoluteSourceFilePath, new Data(doc, firstTime ? -1 : parentDependencyCount + 1));
+
+			var directoryOfCurrentFile = Path.GetDirectoryName(absoluteSourceFilePath) ?? String.Empty;
+
+			// call PrepareDocuments() for each merged dictionary
+			foreach (var dict in doc.Root.Descendants(defaultNameSpace + resDictString))
+			{
+				var sourceAttributeValue = dict.Attribute("Source").Value;
+				var otherComponentReference = "/" + projectName + ";component/";
+				string relativePath;
+				if (sourceAttributeValue.Contains(otherComponentReference))
+				{
+					relativePath = sourceAttributeValue.Replace(otherComponentReference, string.Empty);
+				}
+				else
+				{
+					var absolutePathOfReferencedFile = Path.GetFullPath(Path.Combine(directoryOfCurrentFile, sourceAttributeValue));
+
+
+					relativePath = GetRelativePath(projectPath, absolutePathOfReferencedFile);
+				}
+				PrepareDocuments(ref documents, projectPath, projectName, relativePath, false, documents[absoluteSourceFilePath].DependencyCount);
+			}
+		}
+
+		public static string GetRelativePath(string fromPath, string toPath)
+		{
+			if (string.IsNullOrEmpty(fromPath))
+			{
+				throw new ArgumentNullException("fromPath");
+			}
+
+			if (string.IsNullOrEmpty(toPath))
+			{
+				throw new ArgumentNullException("toPath");
+			}
+
+			Uri fromUri = new Uri(AppendDirectorySeparatorCharIfNecessary(fromPath));
+			Uri toUri = new Uri(AppendDirectorySeparatorCharIfNecessary(toPath));
+
+			if (fromUri.Scheme != toUri.Scheme)
+			{
+				return toPath;
+			}
+
+			Uri relativeUri = fromUri.MakeRelativeUri(toUri);
+			string relativePath = Uri.UnescapeDataString(relativeUri.ToString());
+
+			if (string.Equals(toUri.Scheme, Uri.UriSchemeFile, StringComparison.OrdinalIgnoreCase))
+			{
+				relativePath = relativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+			}
+
+			return relativePath;
+		}
+
+		private static string AppendDirectorySeparatorCharIfNecessary(string path)
+		{
+			if (Path.HasExtension(path) || path.EndsWith(Path.DirectorySeparatorChar.ToString()))
+			{
+				return path;
+			}
+
+			return path + Path.DirectorySeparatorChar;
+		}
+	}
 }
